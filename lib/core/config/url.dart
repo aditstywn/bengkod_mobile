cdef8ac9dea46b73275ccd65c0a85ce88aaf76cf
unknown <restuadityasetyawan@gmail.com>
2025-04-15 10:54:22 +0700

    update : package and base_url

--- lib/core/config/url.dart
+++ lib/core/config/url.dart
@@ -1,6 +1,7 @@
 class Url {
   // development
   // static const String baseUrl = 'https://bengkod-api-dev.rayhanashlikh.my.id';
+  static const String baseUrl = 'https://devbengkod.natagw.my.id';
   // production
-  static const String baseUrl = 'https://bengkod-api.rayhanashlikh.my.id';
+  // static const String baseUrl = 'https://bengkod-api.rayhanashlikh.my.id';
 }
